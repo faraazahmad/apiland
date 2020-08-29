a0cd75dde563b769912ce86ee8d13e0eb2aaa52d
Syed Faraaz Ahmad <faraaz98@live.com>
2020-08-29 16:05:47 +0530

    add hero at home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <section class="hero is-white">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns is-vcentered">
+            <div class="column">
+              <h1 class="title">API Land</h1>
+              <h2 class="subtitle">Public APIs to help you build your next side project</h2>
+
+              <router-link to="/explore" class="button is-warning">Explore APIs</router-link>
+            </div>
+            <div class="column is-narrow">
+              <img id="side-image" src="@/assets/web_dev.svg" alt="web developer">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <div class="container">
+      <h1 class="subtitle"></h1>
+    </div>
   </div>
 </template>
+
+<style scoped>
+#side-image {
+  height: 15rem;
+}
+</style>
