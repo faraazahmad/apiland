2793fb5703c7673fb9e7bd764e607ec80d7f4ce3
suyashchauhan <suyashchauhan31@gmail.com>
2020-09-01 20:49:48 +0530

    Solved the visual bug when loading APIs list

--- src/router/index.ts
+++ src/router/index.ts
@@ -22,6 +22,9 @@ const routes: Array<RouteConfig> = [
 
 const router = new VueRouter({
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    return { x: 0, y: 0 };
+  },
 });
 
 export default router;
